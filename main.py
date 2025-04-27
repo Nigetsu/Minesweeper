2a03a0dd3547397b9b97f47696ee5f4d3e6bd525
Alexey Stenyakin <astenakin@gmail.com>
2025-04-28 01:17:44 +0300

    third commit

--- main.py
+++ main.py
@@ -1,137 +1,185 @@
 from random import randint
+from typing import List, Tuple
 
 
-class Minesweeper:
+class Cell:
+    def __init__(self):
+        self.is_mine: bool = False
+        self.is_open: bool = False
+        self.mine_around: int = 0
+
+    def __str__(self) -> str:
+        return str(self.mine_around)
+
+    def get_value(self) -> int:
+        return self.mine_around
+
+
+class GameField:
     def __init__(self, n: int = 4, m: int = 4, mine_count: int = 4):
         self.__n = n
         self.__m = m
         self.mine_count = mine_count
-        self.board = [[0 for _ in range(m)] for _ in range(n)]
-        self.opened_cells = [[False for _ in range(m)] for _ in range(n)]
-        self.end = False
-        self.__first_cell = True
-
+        self.board: List[List[Cell]] = [[Cell() for _ in range(m)] for _ in range(n)]
+        self.end: bool = False
+        self.__first_cell: bool = True
         self._generate_mine_on_board()
-        self._calculate_mines()
+
+    @property
+    def n(self) -> int:
+        return self.__n
+
+    @property
+    def m(self) -> int:
+        return self.__m
 
     def _generate_mine_on_board(self):
         mine_located = 0
         while mine_located < self.mine_count:
             x = randint(0, self.__m - 1)
             y = randint(0, self.__n - 1)
-            if self.board[y][x] != -1:
-                self.board[y][x] = -1
+            if not self.board[y][x].is_mine:
+                self.board[y][x].is_mine = True
                 mine_located += 1
+        self._calculate_mines()
 
     def _calculate_mines(self):
         for y in range(self.__n):
             for x in range(self.__m):
-                if self.board[y][x] == -1:
+                if self.board[y][x].is_mine:
                     continue
                 count = 0
                 for i in [-1, 0, 1]:
                     for j in [-1, 0, 1]:
                         if (i != 0 or j != 0) and (0 <= i + y < self.__n and 0 <= j + x < self.__m) and (
-                                self.board[i + y][j + x] == -1):
+                                self.board[i + y][j + x].is_mine):
                             count += 1
-                self.board[y][x] = count
+                self.board[y][x].mine_around = count
 
     def _move_mine(self, x: int, y: int):
-        self.board[y][x] = 0
-
+        self.board[y][x].is_mine = False
         while True:
             nx = randint(0, self.__m - 1)
             ny = randint(0, self.__n - 1)
-            if self.board[ny][nx] != -1 and (nx != x and ny != y):
-                self.board[ny][nx] = -1
+            if not self.board[ny][nx].is_mine and (nx != x and ny != y):
+                self.board[ny][nx].is_mine = True
                 break
-
         self._calculate_mines()
 
-    def open(self, x: int, y: int) -> bool:
+    def open_cell(self, x: int, y: int) -> bool:
         if not (0 <= x < self.__m and 0 <= y < self.__n):
             print("Выход за поле")
             return False
 
-        if self.__first_cell and self.board[y][x] == -1:
+        cell = self.board[y][x]
+
+        if self.__first_cell and cell.is_mine:
             self._move_mine(x, y)
+            cell = self.board[y][x]
 
         self.__first_cell = False
 
-        if self.opened_cells[y][x]:
+        if cell.is_open:
             return True
 
-        self.opened_cells[y][x] = True
+        cell.is_open = True
 
-        if self.board[y][x] == -1:
+        if cell.is_mine:
             self.end = True
             return False
-        elif self.board[y][x] == 0:
+        elif cell.mine_around == 0:
             for i in [-1, 0, 1]:
                 for j in [-1, 0, 1]:
                     if (0 <= i + y < self.__n) and (0 <= j + x < self.__m):
-                        self.open(j + x, i + y)
+                        self.open_cell(j + x, i + y)
         return True
-
-    def show(self):
-        print(" ".join([str(i) for i in range(self.__m + 1)]))
-        for y in range(self.__n):
-            print(y + 1, end=" ")
-            for x in range(self.__m):
-                if not self.opened_cells[y][x]:
-                    print("■", end=" ")
-                elif self.board[y][x]:
-                    print(self.board[y][x], end=" ")
-                else:
-                    print(" ", end=" ")
-            print()
 
     def end_game(self) -> bool:
         for y in range(self.__n):
             for x in range(self.__m):
-                if self.board[y][x] != -1 and not self.opened_cells[y][x]:
+                if not self.board[y][x].is_mine and not self.board[y][x].is_open:
                     return False
         return True
 
 
-while True:
-    try:
-        print("Задайте размер поля и кол-во мин: \nНапример: 2 2 1 ")
-        height, width, count = map(int, input().split())
-        if height <= 0 or width <= 0 or count <= 0:
-            print("Числа должны быть положительные и больше нуля")
-            continue
-        if count >= height * width:
-            print("Мин не может быть столько")
-            continue
-        break
-    except ValueError:
-        print("Только 3 числа через пробел")
+class ConsoleInterface:
+    @staticmethod
+    def get_params() -> Tuple[int, int, int] | None:
+        while True:
+            try:
+                height, width, count = map(int, input("Задайте размер поля и кол-во мин: \nНапример: 2 2 1 \n").split())
+                if height <= 0 or width <= 0 or count <= 0:
+                    print("Числа должны быть положительные и больше нуля")
+                    continue
+                if count >= height * width:
+                    print("Мин не может быть столько")
+                    continue
+                return height, width, count
+            except ValueError:
+                print("Только 3 числа через пробел")
 
-game = Minesweeper(n=height, m=width, mine_count=count)
+    @staticmethod
+    def get_command() -> List[str] | None:
+        while True:
+            cmd = list(input("Введите одну из команд:'open x y', 'exit', 'show' \n").split())
+            if not cmd:
+                continue
+            if cmd[0] == "exit":
+                return cmd
+            if cmd[0] == "show":
+                return cmd
+            if cmd[0] == "open":
+                try:
+                    x = int(cmd[1])
+                    y = int(cmd[2])
+                    return cmd
+                except ValueError:
+                    print("Нужно 2 числа")
+                    continue
+            print("Неккоректная команда)")
 
-while not game.end:
-    print("Введите одну из команд:'open x y', 'exit', 'show' ")
-    i = list(input().split())
+    @staticmethod
+    def show(game: GameField):
+        print(" ".join([str(i) for i in range(game.m + 1)]))
+        for y in range(game.n):
+            print(y + 1, end=" ")
+            for x in range(game.m):
+                cell = game.board[y][x]
+                if not cell.is_open:
+                    print("■", end=" ")
+                elif cell.get_value():
+                    print(cell.get_value(), end=" ")
+                else:
+                    print(" ", end=" ")
+            print()
 
-    if i[0] == "exit":
-        print("Вы вышли")
-        game.end = True
 
-    if i[0] == "show":
-        game.show()
+def main():
+    height, width, count = ConsoleInterface.get_params()
+    game = GameField(n=height, m=width, mine_count=count)
 
-    try:
-        if i[0] == "open":
-            if not game.open(int(i[1]) - 1, int(i[2]) - 1):
-                print("Бууум")
-                game.end = True
-            print("Карта:")
-            game.show()
-    except ValueError:
-        print("Пример команды:'open {цифра} {цифра}'")
-        continue
+    while not game.end:
+        ConsoleInterface.show(game)
+        cmd = ConsoleInterface.get_command()
 
-    if game.end_game():
-        print("Вы победили")
-        game.end = True
+        if cmd[0] == "exit":
+            print("Вы вышли")
+            break
+
+        if cmd[0] == "show":
+            ConsoleInterface.show(game)
+
+        if cmd[0] == "open":
+            x, y = map(int, cmd[1:])
+            if not game.open_cell(x - 1, y - 1):
+                print("БУМ!!!")
+                ConsoleInterface.show(game)
+                break
+
+        if game.end_game():
+            print("Вы победили")
+            break
+
+
+if __name__ == "__main__":
+    main()
